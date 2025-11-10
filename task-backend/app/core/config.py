d83615e27e29e9124f1d728627fc08be89bcb39c
jbrayner123 <jbrayner123@gmail.com>
2025-11-10 17:39:17 -0500

    actualizacion CORS

--- task-backend/app/core/config.py
+++ task-backend/app/core/config.py
@@ -3,6 +3,6 @@ import os
 ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://task-frontend.onrender.com",  # Agregar tu dominio de Render
-  "*"  # Solo para testing, luego quitalo
+  "https://proyectogestorfinalrender.onrender.com",
+  "*"  # Temporal para testing, luego puedes quitarlo
 ]
